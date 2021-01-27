ccabf2d56907e07e11005834a9a2ec9931244ef6
jorge-pro <jorge.rs.poa@hotmail.com>
2021-01-27 14:03:54 +0100

    App Calculadora IMC - 26/01/2021

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="label-title">Calculo do IMC</h1>
+  <h2 class="label-subtitle">Digite seu peso e altura para calcular o seu IMC</h2>
+  
+  <div class="div-imc">
+      <span class="p-float-label">
+        <InputText id="input-weight" type="text" v-model="weight" />
+        <label for="input-weight">Peso</label>
+      </span>
+  </div>
+
+  <div class="div-imc">
+      <span class="p-float-label">
+        <InputText id="input-height" type="text" v-model="height" />
+        <label for="input-height">Altura</label>
+      </span>
+  </div>
+
+  <div class="div-imc">
+      <Button label="Calcular" @click="calculate" />
+  </div>
+
+  <div>
+    <p>Seu IMC é: {{imc}}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      height: null,
+      weight: null,
+      imc: null
+    }
+  }, 
+methods: {
+  calculate: function(){
+    this.imc = (this.weight / (this.height * this.height)).toFixed(2);
+    console.log(this.imc);
   }
+}
 }
 </script>
 
@@ -19,8 +48,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.div-imc{
+  margin-top: 2rem ;
+}
+.label-title {
+  font-size: 2rem;
+}
+.label-subtitle {
+  font-size: 1.1rem;
 }
 </style>
